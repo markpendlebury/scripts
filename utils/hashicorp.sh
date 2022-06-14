0512b995f88c478ae1b9bb399b3268c0840ef32c
Mark Pendlebury <mpendlebury@and.digital>
2022-06-14 21:51:50 +0100

    sanitisation and code cleanup

--- utils/hashicorp.sh
+++ utils/hashicorp.sh
@@ -1,9 +1,14 @@
-#!/bin/zsh
+#!/bin/bash
+
+# This function calles my hashiversion api which 
+# returns the latest version number for a 
+# specified hashicorp product
 
 hashicorp.version() {
     if [ $# -ne 1 ]; then
         echo "Usage: hashicorp.version <version>"
     else
-        curl -s https://hashiversion.elesoft.dev/$1
+        curl -s https://hashiversion.elesoft.co.uk/$1
     fi
 }
+
