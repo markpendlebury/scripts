03cbf9ccbffc8cc3a74e7b42fde43bbd04ecf4de
Mark Pendlebury <mpendlebury@and.digital>
2022-06-14 21:49:39 +0100

    slimmed down and removed old code

--- alias.sh
+++ alias.sh
@@ -1,18 +1,7 @@
-#!/bin/zsh
+#!/bin/bash
 
 BASEDIR=~/.scripts
 
-
-
-# Source all the required sub scripts
-for FILE in $BASEDIR/utils/*.sh; do 
-    source $FILE; 
+for FILE in "$BASEDIR"/utils/*.sh; do
+    source "$FILE"
 done
-
-
-# Source client stuff:
-for FILE in $BASEDIR/clients/*.sh; do
-    source $FILE;
-done
-
-
