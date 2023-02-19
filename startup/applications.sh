073bdfe38ea5d28fa82f29484b72f66c2eda4802
Mark Pendlebury <mpendlebury@and.digital>
2023-02-19 11:58:34 +0000

    Added applications.sh
    allows managing of applications opening on login on a schedule

---
+++ startup/applications.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# This script helps with opening application on a schedule,
+# For example, only open slack on a week day during working hours
+
+# Globals
+CURRENT_TIME=$(date +%H:%M)
+
+startup() {
+    # Is today a weekday?
+     if [[ $(date +%u) -lt 6 ]]; then
+        # Is the current time between 6am and 4pm?
+        if [[ "$CURRENT_TIME" > "06:00" ]] || [[ "$CURRENT_TIME" < "16:00" ]]; then
+            # Load the work profile
+            work
+        fi
+     fi
+}
+
+work(){
+    # open slack
+    /usr/bin/slack -u %U
+}
+
+startup
